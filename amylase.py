25a095f0d92684261bfccbae9ce61b7bfd71e0d7
LakshayVerma <lakshay.v4@gmail.com>
2021-01-20 17:24:25 -0500

    Amylase model updated

--- amylase.py
+++ amylase.py
@@ -10,6 +10,8 @@ Created on Fri Nov 13 23:32:20 2020
 PACKAGES
 Import all relevant packages for analytics
 '''
+
+from pathlib import Path
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -20,21 +22,23 @@ from Helper import modelHelper
 def substrateModelCSTR(S):
     return initSubstrateConc - S - residenceTime*(vMax*S)/(bindingAffinity+S)
 
-amylaseHelper = modelHelper.modelHelper(kmOne=69, tempOne=69, kmTwo=69, tempTwo=69)
-
-constDF = pd.read_csv('constantValues.csv')    
+constDF = pd.read_csv(Path("Constants") / "amylaseConstants.csv")   
 dfToArr = constDF.to_numpy()
+
+amylaseHelper = modelHelper.modelHelper(kmOne=dfToArr[5][0], tempOne=dfToArr[5][5], 
+                                        kmTwo=dfToArr[9][0], tempTwo=dfToArr[9][5])
+
 vectorModel = np.vectorize(amylaseHelper.GetRateFromMichaelisModelReaction)
 amylaseReactions = []
 
-S_amylase = np.linspace(0.0001, 0.00005, 100)
+S_amylase = np.linspace(2e-7, 5e-7, 100)
 
-for i in range(0, 5):
-    amylaseReactions.append(vectorModel(dfToArr[4][i+2], S_amylase, dfToArr[2][i+2]))
+for i in range(5, 10):
+    amylaseReactions.append(vectorModel(dfToArr[i][1], S_amylase, dfToArr[i][0]))
 
 fig = plt.figure()
-for i in range(0, 5):
-    plt.plot(S_amylase, amylaseReactions[i], label=str(dfToArr[0][i+2]))
+for i in range(5, 10):
+    plt.plot(S_amylase, amylaseReactions[i-5], label=str(dfToArr[i][4]) + '($^\circ$C)')
 
 plt.xlabel('Substrate Concentration [M]')
 plt.ylabel('Reaction Rate [M/min]')
@@ -42,16 +46,24 @@ plt.title('Modelling of Amylase Reaction', fontweight='bold')
 plt.legend()
 plt.show()
 
+#Use Clausius Clapeyron with given datapoints to determine activation energy
+
+activationEnergy = amylaseHelper.GetActivationEnergyFromClausiusClapeyron()
+reactorTemp = 70+273.15
+kmAtReactorConditions = amylaseHelper.GetDesiredKmFromActivationEnergy(kmKnown=dfToArr[5][0], tempKnown=dfToArr[5][5],
+                                                                       tempDesired = reactorTemp, activationEnergy=activationEnergy)
+print(kmAtReactorConditions)
+
 '''
 MASS BALANCE
 '''
- 
-initSubstrateConc = 5/58400
-flowrate = 5 #(L/min)
-volumeOfReactor = 5 #L
-vMax = 2.3004599999999998e-08 #(M/min)
-bindingAffinity = 4.29e-05 #(M)
 
-residenceTime = volumeOfReactor/flowrate
+initSubstrateConc = 100/58400 # 5 g/L Starch
+#flowrate = 5 #(L/min)
+#volumeOfReactor = 5 #L
+vMax = dfToArr[5][1] #(M/min)
+bindingAffinity = dfToArr[5][0] #(M)
+
+residenceTime = 1000
 
 substrateModellingEquation = fsolve(substrateModelCSTR, 0.000001)
