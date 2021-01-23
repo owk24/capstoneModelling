21e8cbb6283468ce40474e93d6dee49b3d53c092
owk24 <cnowenkim@gmail.com>
2021-01-22 22:18:36 -0500

    second commit

--- invertase.py
+++ invertase.py
@@ -1,16 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Jan 04 21:32:35 2021
-
-@author: Lakshay Verma and Owen Kim
-
-"""
-
-'''
-PACKAGES
-Import all relevant packages for analytics
-'''
-
 from pathlib import Path
 import numpy as np
 import matplotlib.pyplot as plt
@@ -27,8 +15,10 @@ tempTwo = constDF["Temperature (K)"][4]
 
 invertaseHelper = modelHelper.modelHelper(kmOne, tempOne, kmTwo, tempTwo)
 
-def model(inputs, t):
-    index = 3
+rangeOfKm = constDF["Km (M)"]
+rangeOfVm = constDF["Vm (M/min)"]
+
+def model(inputs, t, index) -> [float, float, float]:
     Vm = constDF["Vm (M/min)"][index]
     Km = constDF["Km (M)"][index]
     r = Vm*inputs[2]/(Km + inputs[2])
@@ -40,21 +30,24 @@ def model(inputs, t):
 
 initialG = 0
 initialF = 0
-initialS = 100/58400 #100g/L, 58400kDa molar mass
+initialS = 100/58040 #100g/L, 58400kDa molar mass
 
 initialConditions = [initialG, initialF, initialS]
 
 t = np.linspace(0, 50, 1000)
 
-odeModel = odeint(model, initialConditions, t)
+for i in range(len(rangeOfKm)):
+    odeModel = odeint(model, initialConditions, t, args=(i,))
 
-plt.plot(t, odeModel[:, 0], label="Glucose")
-plt.plot(t, odeModel[:, 1], label="Fructose")
-plt.plot(t, odeModel[:, 2], label="Sucrose")
-plt.xlabel("Time (min)")
-plt.ylabel("Concentration (M)")
-plt.legend(loc="best")
-plt.show()
+    plt.plot(t, odeModel[:, 0], label="Glucose")
+    plt.plot(t, odeModel[:, 1], label="Fructose")
+    plt.plot(t, odeModel[:, 2], label="Sucrose")
+    plt.xlabel("Time (min)")
+    plt.ylabel("Concentration (M)")
+    plt.title("Temp={0:.2f} C, Km={1:.2e} M, Vm={2:.2e} M/min"
+              .format(constDF["Temperature (C)"][i], constDF["Km (M)"][i], constDF["Vm (M/min)"][i]))
+    plt.legend(loc="best")
+    plt.show()
 
 # def substrateModelCSTR(S):
 #     return initialSubstrateConc - S - residenceTime*(vMax*S)/(kM+S)
