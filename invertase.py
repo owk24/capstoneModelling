f8ef22054e99110dca1f5abaf021975aa90314c4
owk24 <cnowenkim@gmail.com>
2021-01-26 00:20:07 -0500

    add useable constants, achieved realistic results

--- invertase.py
+++ invertase.py
@@ -3,10 +3,10 @@ from pathlib import Path
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from scipy.optimize import fsolve
 from scipy.integrate import odeint
 from Helper import modelHelper
 
+concentrationOfComponents = []
 constDF = pd.read_csv(Path("Constants") / "InvertaseConstants.csv")
 kmOne = constDF["Km (M)"][3]
 tempOne = constDF["Temperature (K)"][3]
@@ -30,32 +30,25 @@ def model(inputs, t, index) -> [float, float, float]:
 
 initialG = 0
 initialF = 0
-initialS = 100/58040 #100g/L, 58.4kDa molar mass
+initialS = 10/342.3 #100g/L, 342.3 g/mol molar mass
 
 initialConditions = [initialG, initialF, initialS]
 
-t = np.linspace(0, 50, 1000)
+t = np.linspace(0, 60, 100)
+i = len(rangeOfKm)-1
+concentrationOfComponents = odeint(model, initialConditions, t, args=(i,))
 
-for i in range(len(rangeOfKm)):
-    odeModel = odeint(model, initialConditions, t, args=(i,))
+plt.plot(t, concentrationOfComponents[:, 0], label="Glucose")
+plt.plot(t, concentrationOfComponents[:, 1], label="Fructose")
+plt.plot(t, concentrationOfComponents[:, 2], label="Sucrose")
+plt.xlabel("Time (min)")
+plt.ylabel("Concentration (M)")
+plt.title("Temp={0:.2f} C, Km={1:.2e} M, Vm={2:.2e} M/min"
+          .format(constDF["Temperature (C)"][i], constDF["Km (M)"][i], constDF["Vm (M/min)"][i]))
+plt.legend(loc="best")
+plt.show()
 
-    plt.plot(t, odeModel[:, 0], label="Glucose")
-    plt.plot(t, odeModel[:, 1], label="Fructose")
-    plt.plot(t, odeModel[:, 2], label="Sucrose")
-    plt.xlabel("Time (min)")
-    plt.ylabel("Concentration (M)")
-    plt.title("Temp={0:.2f} C, Km={1:.2e} M, Vm={2:.2e} M/min"
-              .format(constDF["Temperature (C)"][i], constDF["Km (M)"][i], constDF["Vm (M/min)"][i]))
-    plt.legend(loc="best")
-    plt.show()
-
-# def substrateModelCSTR(S):
-#     return initialSubstrateConc - S - residenceTime*(vMax*S)/(kM+S)
-# initialSubstrateConc = 10000/(270*1000) #100g/L, molar mass: 270 2Da
-# kM = constDF["Km (M)"][3]
-# vMax = constDF["Vm (M/min)"][3]
-# residenceTime = 10 # minutes
-
-# finalSubstrateConcentration = fsolve(substrateModelCSTR, 0.005)
-# print(finalSubstrateConcentration)
-
+concentrationDf = pd.DataFrame({"Glucose": concentrationOfComponents[:, 0],
+                                "Fructose": concentrationOfComponents[:, 1],
+                                "Sucrose": concentrationOfComponents[:, 2]})
+print(concentrationDf["Glucose"].iloc[-1]*342.3)
