9dc774392c7c53151b8cbb0555eeb4f2e5b9af1c
owk24 <cnowenkim@gmail.com>
2021-02-02 16:31:11 -0500

    change DF name and allow for dynamic paths

--- invertase.py
+++ invertase.py
@@ -6,6 +6,7 @@ import pandas as pd
 from Helper.ModelHelper import ModelHelper
 
 invertaseModelHelper = ModelHelper()
+invertaseModelHelper.CreateFoldersForOutput("Invertase")
 constDF = pd.read_csv(Path("Constants") / "InvertaseConstants.csv")
 
 initialEnzyme = 9E-09
@@ -20,18 +21,18 @@ initialS = 10/342.3 #100g/L, 342.3 g/mol molar mass
 initialConditions = [initialG, initialF, initialS]
 
 t = np.linspace(0, 480, 100)
-test = invertaseModelHelper.GetRatesVersusTimeFromModel(t, km, vm, initialConditions)
+invertaseModelOutput = invertaseModelHelper.GetRatesVersusTimeFromModel(t, km, vm, initialConditions)
 
 exportTitle = "Invertase_T={0:.0f}C_Km={1:.2e}_Vm={2:.2e}".format(temperature, km, kcat)
-plt.plot(t, test["Glucose [M]"], label="Glucose")
-plt.plot(t, test["Fructose [M]"], label="Fructose")
-plt.plot(t, test["Sucrose [M]"], label="Sucrose")
+plt.plot(t, invertaseModelOutput["Glucose [M]"], label="Glucose")
+plt.plot(t, invertaseModelOutput["Fructose [M]"], label="Fructose")
+plt.plot(t, invertaseModelOutput["Sucrose [M]"], label="Sucrose")
 plt.xlabel("Time [min]")
 plt.ylabel("Concentration [M]")
 plt.title("Temp={0:.2f}C, Km={1:.2e}M, Vm={2:.2e}M/min".format(temperature, km, kcat))
 plt.legend(loc="best")
-plt.savefig(Path("Outputs/Invertase/Figures") /
+plt.savefig(Path("Output/Invertase/Graphs") /
             "Invertase_T={0:.0f}C_Km={1:.2e}_Vm={2:.2e}.png".format(temperature, km, kcat))
 plt.show()
-test.to_csv(Path("Outputs/Invertase/CSVs") /
+invertaseModelOutput.to_csv(Path("Output/Invertase/CSVs") /
             "Invertase_T={0:.0f}C_Km={1:.2e}_Vm={2:.2e}.csv".format(temperature, km, kcat))
